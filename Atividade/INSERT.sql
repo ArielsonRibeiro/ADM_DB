4442b3681cbdbbd8a577a4cc0407bf0636780962
Zero <arielson240997@gmail.com>
2019-09-22 11:26:29 -0300

    QUESTAO_20_27

--- Atividade/INSERT.sql
+++ Atividade/INSERT.sql
@@ -1,23 +1,41 @@
 Use SIGAA;
 
+INSERT INTO Cursos VALUES (1, 120, 'Matematica Computacional', NULL);
+INSERT INTO Cursos VALUES (2, 120, 'Matematica ', NULL);
+INSERT INTO Cursos VALUES (3, 120, 'fisica', NULL);
+INSERT INTO Cursos VALUES (4, 120, 'Portugues', NULL);
+
 Insert into Disciplinas VALUES(1 , 2, 'Matematica');
 Insert into Disciplinas VALUES(2 , 2, 'MatematicaII');
+Insert into Disciplinas VALUES(3 , 2, 'MatematicaIII');
+
+Insert into Disciplinas VALUES(4 , 2, 'Calculo I');
+Insert into Disciplinas VALUES(5 , 1, 'MatematicaI');
+Insert into Disciplinas VALUES(6 , 1, 'MatematicaII');
+Insert into Disciplinas VALUES(7 , 1, 'MatematicaIII');
 
 
 INSERT INTO Pre_Requisitos VALUES(2, 1);
+INSERT INTO Pre_Requisitos VALUES(3, 2);
+INSERT INTO Pre_Requisitos VALUES(3, 4);
 
-INSERT INTO Cursos VALUES (1, 120, 'Matematica Computacional', NULL);
-INSERT INTO Cursos VALUES (2, 120, 'Matematica Computacional', NULL);
-INSERT INTO Cursos VALUES (3, 120, 'Matematica Computacional', NULL);
-INSERT INTO Cursos VALUES (4, 120, 'Portugues', NULL);
+INSERT Curriculos VALUES(2, 1, 1)
+INSERT Curriculos VALUES(2, 2, 2)
+INSERT Curriculos VALUES(2, 3, 3)
+INSERT Curriculos VALUES(2, 4, 2)
+
+INSERT Curriculos VALUES(1, 5, 1)
+INSERT Curriculos VALUES(1, 6, 2)
+INSERT Curriculos VALUES(1, 7, 3)
+
 
 INSERT INTO Professores(COD_CURSO, COD_PROF, NOM_PROF) VALUES(1, 1, 'JEAN');
 INSERT INTO Professores(COD_CURSO, COD_PROF, NOM_PROF) VALUES(1, 2, 'JOAO');
-INSERT INTO Professores(COD_CURSO, COD_PROF, NOM_PROF) VALUES(3, 4, 'Pedro');
-INSERT INTO Professores(COD_CURSO, COD_PROF, NOM_PROF) VALUES(3, 5, 'Lauro');
+INSERT INTO Professores(COD_CURSO, COD_PROF, NOM_PROF) VALUES(2, 3, 'Pedro');
+INSERT INTO Professores(COD_CURSO, COD_PROF, NOM_PROF) VALUES(2, 4, 'Lauro');
 
 
-UPDATE Cursos SET COD_COORD = 1 WHERE COD_CURSO = 2;
+UPDATE Cursos SET COD_COORD = 4 WHERE COD_CURSO = 2;
 
 UPDATE Cursos SET COD_COORD = 4 WHERE COD_CURSO = 1; -- Tem Que Gerar um Erro(trigger)
 
@@ -26,9 +44,11 @@ INSERT INTO Alunos(MAT_ALU, NOM_ALU, COD_CURSO, DAT_NASC, MGP, TOT_CRED )
 INSERT INTO Alunos(MAT_ALU, NOM_ALU, COD_CURSO, DAT_NASC, MGP, TOT_CRED )
     VALUES (1235, 'PAULO', 1, '12-02-2002', 0, 0)
 
+INSERT INTO Alunos(MAT_ALU, NOM_ALU, COD_CURSO, DAT_NASC, MGP, TOT_CRED )
+    VALUES (1236, 'PAULO_2',2, '12-02-2002', 0, 0)
 
-INSERT Curriculos VALUES(1,1, 1)
-INSERT Curriculos VALUES(1,2, 1)
+
+
 
 INSERT Historicos_Escolares(ANO       
 , SEMESTRE
@@ -49,11 +69,35 @@ INSERT Historicos_Escolares(ANO
 , SITUACAO) VALUES (
     2019, 1, 1, 1235, 6.0, 22, 'RP'
 );
+SELECT * FROM Disciplinas
+INSERT INTO Turmas(ANO
+, SEMESTRE
+, COD_DISC
+, TURMA   
+, TOT_VAGAS
+, VAG_OCUP
+, COD_PROF) VALUES(2019, 1,5, 'MA1', 20, 10, 1)
+
+INSERT INTO Turmas(ANO
+, SEMESTRE
+, COD_DISC
+, TURMA   
+, TOT_VAGAS
+, VAG_OCUP
+, COD_PROF) VALUES(2019, 1,6, 'MA1', 20, 10, 1)
+
+INSERT INTO Turmas(ANO
+, SEMESTRE
+, COD_DISC
+, TURMA   
+, TOT_VAGAS
+, VAG_OCUP
+, COD_PROF) VALUES(2019, 2,7, 'MA1', 20, 10, 1)
+
 
 
 INSERT INTO Turmas(ANO
 , SEMESTRE
-
 , COD_DISC
 , TURMA   
 , TOT_VAGAS
@@ -74,29 +118,57 @@ INSERT INTO Tumas_Matriculadas(ANO
 , SEMESTRE
 , COD_DISC
 , TURMA
-, MAT_ALU) VALUES(2019, 1, 2, 'MA1', 1234)
+, MAT_ALU) VALUES(2019, 1, 2, 'MA2', 1234) -- Erro Trigger requisitos
+
+INSERT INTO Tumas_Matriculadas(ANO 
+, SEMESTRE
+, COD_DISC
+, TURMA
+, MAT_ALU) VALUES(2019, 1, 1, 'MA1', 1236) -- Erro Trigger Validacao de Disciplina do curso
 
 
 INSERT INTO Tumas_Matriculadas(ANO 
 , SEMESTRE
 , COD_DISC
 , TURMA
-, MAT_ALU) VALUES(2019, 2, 1, 'MA4', 1234)
+, MAT_ALU) VALUES(2019, 1, 1, 'MA1', 1236)
 
 INSERT INTO Tumas_Matriculadas(ANO 
     , SEMESTRE
     , COD_DISC
     , TURMA
-    , MAT_ALU) VALUES(2019, 1, 2, 'MA2', 1234) -- tem que dar erro de trigger;
-
-DELETE FROM Tumas_Matriculadas
+    , MAT_ALU) VALUES(2019, 1, 2, 'MA2', 1234) -- tem que dar erro de trigger Disciplina nao pertence ao curso do aluno.;
 
 
-Insert into Disciplinas VALUES(3 , 5, 'MatematicaIII');
+INSERT INTO Tumas_Matriculadas(ANO 
+, SEMESTRE
+, COD_DISC
+, TURMA
+, MAT_ALU) VALUES(2019, 1, 5, 'MA1', 1234) 
+INSERT INTO Tumas_Matriculadas(ANO 
+, SEMESTRE
+, COD_DISC
+, TURMA
+, MAT_ALU) VALUES(2019, 1, 6, 'MA1', 1234)  --eRROR tIGRES q20.
+--DELETE FROM Tumas_Matriculadas
 
-Insert into Disciplinas VALUES(4 , 5, 'português I');
+--INSERT Curriculos VALUES(2, 4, 1)
+UPDATE Tumas_Matriculadas
+    SET NOTA_3 = 10
+    WHERE MAT_ALU = 1234 -- ERROR TRIGRE
 
-INSERT Curriculos VALUES(2, 4, 1)
+UPDATE Tumas_Matriculadas
+    SET NOTA_3 = 10, NOTA_2 = 10, NOTA_1 = 10
+    WHERE MAT_ALU = 1234
+
+UPDATE Tumas_Matriculadas
+    SET NOTA_4 = 10
+    WHERE MAT_ALU = 1234 -- ERROR TRIGRE
+
+UPDATE Tumas_Matriculadas
+    SET FALTAS_2 = 10
+    WHERE MAT_ALU = 1234
+
 
 INSERT INTO Turmas(ANO
 , SEMESTRE
@@ -121,11 +193,6 @@ UPDATE Historicos_Escolares SET MEDIA = 7 WHERE MAT_ALU = 1234;
 
 delete Tumas_Matriculadas
 
-INSERT INTO Tumas_Matriculadas(ANO 
-, SEMESTRE
-, COD_DISC
-, TURMA
-, MAT_ALU) VALUES(2019, 1, 4, 'MA2', 1234)
 
 
 
--- Atividade/QUestao01_Trigger.sql
+++ Atividade/QUestao01_Trigger.sql
@@ -6,17 +6,27 @@
 CREATE TRIGGER TG_ALTER_GRADE_CURSO_CALCULAR_CREDITOS ON [Curriculos]
     FOR INSERT, UPDATE, DELETE 
 AS
-    BEGIN
-        UPDATE SIGAA.dbo.[Cursos]
-            SET TOT_CRED = (Select SUM(disc.QTD_CRED)
-                FROM Curriculos AS cur 
-                RIGHT JOIN Disciplinas AS disc 
-                ON disc.COD_DISC = cur.COD_DISC
-                WHERE CUR.COD_CURSO = (SELECT COD_CURSO FROM INSERTED))
-        WHERE COD_CURSO = (SELECT COD_CURSO FROM INSERTED)
-                
+BEGIN
+    DECLARE
+    @COD_CURSO INT;
+    IF( (SELECT count(*) FROM inserted) > 0)
+        BEGIN
+           SELECT @COD_CURSO = COD_CURSO FROM inserted;    
+        END
+    ELSE
+        BEGIN
+           SELECT @COD_CURSO = COD_CURSO FROM deleted;        
+        END
 
-    END
+    UPDATE SIGAA.dbo.[Cursos]
+        SET TOT_CRED = (Select SUM(disc.QTD_CRED)
+            FROM Curriculos AS cur 
+            RIGHT JOIN Disciplinas AS disc 
+            ON disc.COD_DISC = cur.COD_DISC
+            WHERE CUR.COD_CURSO = (SELECT COD_CURSO FROM INSERTED))
+    WHERE COD_CURSO = @COD_CURSO;
+    
+END
 GO
 
 --2. A descrição do curso deve ser armazenada em caixa alta (maiúscula);
@@ -182,12 +192,6 @@ AS
                 SELECT @VAG_OCUP = @VAG_OCUP - 1;
             END
 
-        PRINT @COD_DISC
-        PRINT @ANO 
-        PRINT @SEMESTRE
-        PRINT @TURMA
-   
-
         UPDATE SIGAA.dbo.Turmas
             SET VAG_OCUP  = @VAG_OCUP
             WHERE COD_DISC = @COD_DISC AND ANO = @ANO AND SEMESTRE = @SEMESTRE AND TURMA = @TURMA;
@@ -240,8 +244,7 @@ AS
                 FROM  Historicos_Escolares he WHERE MAT_ALU = @MAT_ALU;
 
             SELECT @MGP = @MGP/@I;
-            PRINT @MGP
-            PRINT @TOT_CRED
+
             UPDATE Alunos
                 SET TOT_CRED = @TOT_CRED
                     , MGP = @MGP
@@ -292,7 +295,7 @@ AS
     SELECT @CONT2 = COUNT(he.COD_DISC) FROM [Historicos_Escolares] he
     INNER JOIN (SELECT * FROM [Pre_Requisitos]
                     WHERE COD_DISC = @COD_DISC) pr    
-        ON he.COD_DISC = pr.COD_DISC
+        ON he.COD_DISC = pr.COD_DISC_PRE
         WHERE UPPER(he.SITUACAO) = 'AP';
 
     IF  (@CONT1 != @CONT2)
@@ -357,4 +360,218 @@ GO
 
 -- 20. O limite máximo de créditos matriculados em um mesmo período é dado pelo período de
 -- maior quantidade de créditos do curso;
--- 21. Sequencialmente, o aluno só pode ter uma nota com o valor nulo, ou seja, a nota
+CREATE TRIGGER TG_VALIDAR_QTD_MAXIMA_CREDITO ON [Tumas_Matriculadas]
+FOR INSERT
+AS
+    BEGIN
+    DECLARE
+        @COD_DISC INT
+        , @PERIODO INT
+        , @MAT_ALU INT
+        , @ANO INT
+        , @SEMESTRE INT
+        , @COD_CURSO INT
+        , @QTD_CRED_MAIOR_PER INT
+        , @QTD_CRED_MAT INT;
+    SELECT @COD_DISC = COD_DISC, @MAT_ALU = MAT_ALU, @ANO = ANO, @SEMESTRE = SEMESTRE FROM inserted;
+
+    SELECT @PERIODO = PERIODO, @COD_CURSO = COD_CURSO FROM Curriculos WHERE COD_DISC = @COD_DISC;
+
+    -- SELECT @QTD_CRED_MAIOR_PER = SUM(D.QTD_CRED) FROM Curriculos C
+    --     INNER JOIN Disciplinas D ON D.COD_DISC = C.COD_DISC
+    --     WHERE (C.PERIODO = @PERIODO AND C.COD_CURSO = @COD_CURSO);
+    
+    SELECT TOP 1 @QTD_CRED_MAIOR_PER = SUM(D.QTD_CRED) FROM Disciplinas D
+    INNER JOIN Curriculos C ON C.COD_DISC = D.COD_DISC
+    WHERE C.COD_CURSO = @COD_CURSO
+    GROUP BY C.PERIODO
+    ORDER BY 1 DESC;
+
+    SELECT @QTD_CRED_MAT = SUM(D.QTD_CRED) FROM Tumas_Matriculadas TM
+        INNER JOIN Disciplinas D ON D.COD_DISC =  TM.COD_DISC
+        WHERE MAT_ALU = @MAT_ALU AND ANO = @ANO AND SEMESTRE = @SEMESTRE;
+    
+    IF(@QTD_CRED_MAIOR_PER < @QTD_CRED_MAT)
+        BEGIN
+        RAISERROR('O ALUNO NÃO PODE SE MATRICULAR NESSA DISCIPLINA, POIS EXCEDE O NÚMERO MAXIMO DE CRÉDITO DE UM MESMO PERÍODO!', 10 , 1 );
+            ROLLBACK;
+        END
+
+    END
+GO
+
+
+-- 21. Sequencialmente, o aluno só pode ter uma nota com o valor nulo, ou seja, a nota 3 não pode
+-- ser preenchida se pelo menos uma das notas anteriores (1 e 2) possuírem valor;
+
+CREATE TRIGGER TG_VALIDAR_ORDEM_NOTAS ON [Tumas_Matriculadas]
+FOR INSERT, UPDATE
+AS
+    IF(UPDATE(NOTA_3))
+        BEGIN
+            DECLARE
+                @N1 NUMERIC(3,1)
+                , @N2 NUMERIC(3,1);
+
+            SELECT @N1 = NOTA_1 FROM inserted;
+            SELECT @N2 = NOTA_2 FROM inserted;
+
+            IF ( @N1 IS NULL AND @N2 IS NULL)
+                BEGIN
+                    RAISERROR('NÃO É POSSÍVEL CADASTRAR A 3º NOTA SEM QUE PELO MENOS UMA NOTA ESTEJA CADASTRADA', 10 , 1 );
+                    ROLLBACK;
+                END
+        END
+    
+GO
+
+-- 22. As faltas devem ser preenchidas sequencialmente, ou seja, as faltas da segunda unidade só
+--  podem ser lançadas quando as da primeira já o tiverem sido;
+
+CREATE TRIGGER TG_VALIDAR_ORDEM_FALTAS ON [Tumas_Matriculadas]
+FOR INSERT, UPDATE
+AS
+    BEGIN
+        DECLARE
+            @F INT;
+        IF(UPDATE(FALTAS_2))
+            BEGIN
+                SELECT @F = FALTAS_1 FROM inserted;
+                IF ( @F IS NULL)
+                    BEGIN
+                        RAISERROR('NÃO É POSSIVEL CADASTRAR AS FALTAS DA 2ª UNIDADES SEM ANTES TERC CADASTRADA DA 1ª', 10 , 1 );
+                        ROLLBACK;
+                    END
+            END
+        IF(UPDATE(FALTAS_3))
+            BEGIN
+                SELECT @F = FALTAS_2 FROM inserted;
+                IF ( @F IS NULL)
+                    BEGIN
+                        RAISERROR('nÃO É POSSIVEL CADASTRAR AS FALTAS DA 3ª UNIDADES SEM ANTES TERC CADASTRADA DA 2ª', 10 , 1 );
+                        ROLLBACK;
+                    END
+            END
+    END
+    
+GO
+-- 23. O valor das faltas em uma unidade não pode exceder a 1/3 do total de aulas da disciplina;
+
+
+-- 24. A quarta nota só pode ser preenchida se uma das anteriores estiver nula;
+
+ALTER TRIGGER TG_PREENCHER_4_NOTA ON [Tumas_Matriculadas]
+FOR INSERT, UPDATE
+AS
+    IF(UPDATE(NOTA_4))
+        BEGIN
+            DECLARE
+                @N1 NUMERIC(3,1)
+                , @N2 NUMERIC(3,1)
+                , @N3 NUMERIC(3,1);
+
+            SELECT @N1 = NOTA_1 FROM inserted;
+            SELECT @N2 = NOTA_2 FROM inserted;
+            SELECT @N3 = NOTA_3 FROM inserted;
+            IF ( (@N1 IS NOT NULL) AND (@N2 IS NOT NULL) AND (@N3 IS NOT NULL))
+                BEGIN
+                    RAISERROR('NÃO É POSSÍVEL CADASTRAR A 4º NOTA, POIS O ALUNO JÁ TEM AS 3 PRIMEIRAS NOTAS CADASTRADAS', 10 , 1 );
+                    ROLLBACK;
+                END
+        END
+    
+GO
+
+-- 25. Não pode ser lançada no histórico uma disciplina em um período em que o pré-requisito da
+-- disciplina não tenha sido cursado em períodos anteriores;
+
+CREATE TRIGGER TG_LANCAR_DISCIPLINA_VALIDAR_PRE_REQ ON [Historicos_Escolares]
+FOR INSERT
+AS
+    BEGIN
+    DECLARE
+        @COD_DISC INT
+        , @CONT1 INT
+        , @CONT2 INT;
+    SELECT @COD_DISC = COD_DISC FROM inserted;
+
+    SELECT @CONT1 = COUNT(*) FROM [Pre_Requisitos] WHERE COD_DISC = @COD_DISC;
+
+    SELECT @CONT2 = COUNT(he.COD_DISC) FROM [Historicos_Escolares] he
+    INNER JOIN (SELECT * FROM [Pre_Requisitos]
+                    WHERE COD_DISC = @COD_DISC) pr    
+        ON he.COD_DISC = pr.COD_DISC_PRE
+        WHERE UPPER(he.SITUACAO) = 'AP';
+
+    IF  (@CONT1 != @CONT2)
+        BEGIN
+            RAISERROR('O ALUNO NÃO POSSUI OS PRÉ-REQUISITOS SUFICIENTE PARA CADASTRAR ESSE DISCIPLINA NO HISTÓRICO', 10 , 1 );
+            ROLLBACK;
+        END
+    END
+GO
+
+-- 26. Só pode existir uma ocorrência com aprovação para uma disciplina do histórico. Além disso,
+-- está ocorrência deve ser cronologicamente a última da disciplina no histórico do aluno;
+CREATE TRIGGER TG_LANCAR_DISCIPLINA_VALIDAR_ALUNO_JA_CURSOU ON [Historicos_Escolares]
+FOR INSERT
+AS
+    BEGIN
+    DECLARE
+        @COD_DISC INT
+        , @MAT_ALU INT;
+    SELECT @COD_DISC = COD_DISC
+            , @MAT_ALU = MAT_ALU
+    FROM inserted;
+
+
+    IF(EXISTS (SELECT * FROM [Historicos_Escolares] WHERE COD_DISC = @COD_DISC AND MAT_ALU = MAT_ALU AND SITUACAO = 'AP'))
+        BEGIN
+        RAISERROR('O ALUNO JÀ CURSOU A DISCIPLINA E TEVE APROVAÇÃO, COM ISSO NÃO É POSSÍVEL CADASTRAR ESSA DISCIPLINA', 10 , 1 );
+            ROLLBACK;
+        END
+
+    END
+GO
+
+-- 27. Não podem ser lançadas disciplinas que não pertençam a grade curricular do aluno;
+CREATE TRIGGER TG_VALIDAR_DISCIPLINA_PERTENCE_AO_CURSO_DO_ALUNO_LANCAR_HISTORICO ON [Historicos_Escolares]
+FOR INSERT
+AS
+    BEGIN
+    DECLARE
+        @COD_DISC INT
+        , @MAT_ALU INT
+        , @CURSO_DISC INT
+        , @CURSO_ALU INT;
+    SELECT @COD_DISC = COD_DISC
+            , @MAT_ALU = MAT_ALU
+    FROM inserted;
+    
+    SELECT @CURSO_DISC = COD_CURSO FROM Curriculos WHERE COD_DISC = @COD_DISC;
+    SELECT @CURSO_ALU = COD_CURSO FROM Alunos WHERE MAT_ALU = @MAT_ALU;
+    
+
+    IF(@CURSO_ALU != @CURSO_DISC)
+        BEGIN
+        RAISERROR('ESTÁ DISCIPLINA NÂO PERTENCE AO CURRICULO DO CURSO DO ALUNO, COM ISSO NÃO PODE SER LANÇADA!', 10 , 1 );
+            ROLLBACK;
+        END
+
+    END
+GO
+
+-- 28. O total de faltas do histórico não pode exceder ao total de aulas da disciplina;
+
+
+-- 29. Deve ser validada a consistência da situação do histórico em relação aos dados de média e
+-- faltas.
+
+CREATE TRIGGER VALIDAR_CONSISTENCIA_HISTORICO_ALU ON [Historicos_Escolares]
+FOR INSERT, UPDATE
+AS
+    
+GO
+
+-- 30. Deve existir uma rotina para finalizar a turma, transferindo os dados da matrícula para o
+-- histórico.
